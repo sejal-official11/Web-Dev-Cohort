e1dad825595733f9759dfa4853e4ce5b208e684f
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-04-23 21:07:11 +0530

    jsbasics

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+ function greets(user) {
+    
+    console.log( "Ms. " + user.firstname + ", " + "your age is " + user.age)
+
+
+
+ }
+ 
+ let user = {
+    firstname: "Sejal",
+    age: 24,
+    gender: "Female"
+ }
+ greets(user);
