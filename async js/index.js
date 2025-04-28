ff0194e8121cc5af7ae2db8fc12e076773bf1edf
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-04-28 22:06:43 +0530

    Promises 1

--- async js/index.js
+++ async js/index.js
@@ -1,34 +1,96 @@
-const user = {
-  name: "Sejal",
-  age: 24,
-};
+// const user = {
+//   name: "Sejal",
+//   age: 24
+// };
 
-class Rectangle {
-  constructor(width, height, color) {
-    this.width = width;
-    this.height = height;
-    this.color = color;
-  }
+// class Rectangle {
+//   constructor(width, height, color) {
+//     this.width = width;
+//     this.height = height;
+//     this.color = color;
+//   }
 
-  area() {
-    const area = this.width * this.height;
-    return area;
-  }
-  paint() {
-    console.log(`Painting with color ${this.color}`);
-  }
+//   area() {
+//     const area = this.width * this.height;
+//     return area;
+//   }
+//   paint() {
+//     console.log(`Painting with color ${this.color}`);
+//   }
+
+//   destroy() {
+
+//   }
+// }
+
+// const rect = new Rectangle(11, 4, "red");
+// const area = rect.area();
+// console.log(area);
+// rect.paint();
+
+// const date = new Date();
+// console.log(date.getMonth());
+
+// const map = new Map();
+// map.set('name', "Alice");
+// map.set('age', 24);
+// console.log(map.get('name'));
+
+// var str = "Sejal";
+
+// console.log(str.toLocaleLowerCase());
+
+// const today = new Date();
+// console.log(today.getFullYear());
+// console.log(today.getSeconds());
+
+// function logName() {
+//     console.log("Hello");
+// }
+
+// setTimeout(logName, 3000);
+
+// function callback(){
+//     console.log("Done");
+// }
+
+// let p = setTimeoutPromisified(3000).then(callback); // return object of promise class
+// console.log(p);
+
+// function setTimeoutPromisified(){
+//     let d = new Promise((res, resp) => (console.log("Hi")));
+//     return d;
+// }
+
+// promisified version of settimeout
+
+// function setTimeoutPromisified(ms) {
+//   let p = new Promise((resolve) => setTimeout(resolve, ms));
+//   return p;
+// }
+
+// function callback() {
+//   console.log("3 seconds have passed");
+// }
+
+// setTimeoutPromisified(3000).then(callback);
 
 
-  destroy() {
+// function random(){
 
-  }
+// }
+// function callback() {
+//     console.log("Hello");
+// }
 
-  
-}
+// let p = new Promise(random);
+// console.log(p);
+// p.then(callback);
 
 
-const rect = new Rectangle(11, 4, "red");
-const area = rect.area();
-console.log(area);
-rect.paint();
- 
+
+
+
+
+
+
