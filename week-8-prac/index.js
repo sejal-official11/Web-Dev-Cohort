78eea0347d4739e9fa8d741f435b6e57dc78cce1
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-07-30 21:25:45 +0530

    Step 2 - Adding skeleton routes

---
+++ week-8-prac/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.post("/user/signup", function (req, res) {
+    res.json({
+        message: "Signup endpoint"
+    })
+
+});
+
+app.post("/user/signin", function (req, res) {
+
+    res.json({
+        message: "Signin endpoint"
+    })
+
+});
+
+app.get("/user/purchases", function(req, res) {
+    // you would expect that you can pay u money
+    res.json({
+        message: "These are the courses where you have enrolled"
+
+    })
+})
+
+app.post("/course/purchase", function(req, res) {
+
+    res.json({
+        message: "courses endpoint"
+    })
+
+});
+
+
+app.get("/courses", function(req, res) {
+    res.json({
+        message: "courses";
+    })
+})
+ 
+app.listen(3000);
