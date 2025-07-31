275d25c1d8a0ad173f73a86a3b3a5a7a844162d8
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-07-31 14:48:17 +0530

    Step 3 adding course router

--- week-8-prac/routes/course.js
+++ week-8-prac/routes/course.js
@@ -1,17 +1,23 @@
-function createCourseRoutes(app) {
-  app.post("/course/purchase", function (req, res) {
+const express = require("express");
+
+const {Router} = require("express");
+
+
+const courseRouter = Router();
+
+  courseRouter.post("/purchase", function (req, res) {
     res.json({
       message: "courses endpoint",
     });
   });
 
-  app.get("/courses", function (req, res) {
+  courseRouter.get("/courses", function (req, res) {
     res.json({
       message: "courses",
     });
   });
-}
+
 
 module.exports = {
-    createCourseRoutes: createCourseRoutes
+    courseRouter: courseRouter
 }
