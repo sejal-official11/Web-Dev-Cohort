1a954ca5df5f6fdc2c1dca1cca3b811af8262f54
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-07-31 14:43:21 +0530

    learn express routing

--- week-8-prac/index.js
+++ week-8-prac/index.js
@@ -1,41 +1,14 @@
 const express = require("express");
 
+const { createUserRoutes } = require("./routes/user");
+const { createCourseRoutes } = require("./routes/course");
+
 const app = express();
-const port = 3000;
 
-app.post("/user/signup", function (req, res) {
-    res.json({
-        message: "Signup endpoint"
-    })
+app.use("/user", userRouter);
+app.use("/course", courseRouter);
 
-});
+// createUserRoutes(app);
+// createCourseRoutes(app);
 
-app.post("/user/signin", function (req, res) {
-
-    res.json({
-        message: "Signin endpoint"
-    })
-
-});
-
-app.get("/user/purchases", function(req, res) {
-    // you would expect that you can pay u money
-    res.json({
-        message: "These are the courses where you have enrolled"
-
-    })
-})
-
-app.post("/course/purchase", function(req, res) {
-    res.json({
-        message: "courses endpoint"
-    })
-});
-
-app.get("/courses", function(req, res) {
-    res.json({
-        message: "courses"
-    })
-})
- 
 app.listen(3000);
