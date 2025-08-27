328a961e6ac6005e8e455c4b0944f98630dc787e
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-08-27 12:22:54 +0530

    Adding secrets in .env file

--- week-8-prac/config.js
+++ week-8-prac/config.js
@@ -1,5 +1,7 @@
-const JWT_USER_PASSWORD = "12345";
-const JWT_ADMIN_PASSWORD = "admin";
+
+
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
 
 
 
--- week-8-prac/index.js
+++ week-8-prac/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require("express");
 const mongoose = require("mongoose");
 const { userRouter } = require("./routes/user");
@@ -18,7 +20,7 @@ app.use("/api/v1/admin", adminRouter)
 
 async function main()
 {
-     await mongoose.connect("")
+     await mongoose.connect(process.env.MONGO_URL)
      app.listen(3000);
      console.log("listening on port 3000");
      
