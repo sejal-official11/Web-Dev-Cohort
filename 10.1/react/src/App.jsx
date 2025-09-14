cf31b6238d1b3433126ef091cc2c519a35bab710
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-09-14 18:24:16 +0530

    Adding useRef

--- 10.1/react/src/App.jsx
+++ 10.1/react/src/App.jsx
@@ -1,89 +1,142 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-  Outlet,
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+//   Link,
+//   useNavigate,
+//   Outlet,
+// } from "react-router-dom";
 
-function App() {
+import { useRef, useState } from "react";
 
-
-//   const routes = [{
-//     path: "/neet/online-coaching-class-11",
-//     element: <Class11Program/>
-
-//   }, 
-// {
-//   path: "",
-//   element: 
-// }]
+// function App() {
 
 
+// //   const routes = [{
+// //     path: "/neet/online-coaching-class-11",
+// //     element: <Class11Program/>
 
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/neet/online-coaching-class-11" element={<Class11Program />}/>
-            <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-            <Route path="/" element={<LandingPage />} />
-            <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+// //   }, 
+// // {
+// //   path: "",
+// //   element: 
+// // }]
+
+
+
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route path="/neet/online-coaching-class-11" element={<Class11Program />}/>
+//             <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+//             <Route path="/" element={<LandingPage />} />
+//             <Route path="*" element={<NoPage />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+// function Layout() {
+//   return (
+//     <div style={{ height: "100vh", background: "green" }}>
+//       <Header />
+//       <div style={{ height: "90vh", background: "red" }}>
+//         <Outlet />
+//       </div>
+//       Footer | Contact us
+//     </div>
+//   );
+// }
+
+// function Header() {
+//   return (
+//     <div>
+//       <Link to="/neet">Allen</Link>|
+//       <Link to="/neet/online-coaching-class-11">Class 11</Link>|
+//       <Link to="/neet/online-coaching-class-12">Class 12</Link>
+//     </div>
+//   );
+// }
+
+// function NoPage() {
+//   return <div>Sorry Page not found</div>;
+// }
+
+// function LandingPage() {
+//   return <div>Welcome to allen</div>;
+// }
+
+// function Class11Program() {
+//   return <div>NEET programs from Class 11th</div>;
+// }
+
+// function Class12Program() {
+//   const navigate = useNavigate();
+
+//   function redirectUser() {
+//     navigate("/");
+//   }
+
+//   return (
+//     <div>
+//       NEET programs from Class 12th
+//       <button onClick={redirectUser}>GO back to Landing Page</button>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// USEREF
+
+
+// function App(){
+//   const inputRef = useRef(null);
+//   let value = 1;
+
+//   // const [a, setA] = useState()
+//   function focusOnInput(){
+//     //  document.getElementById("name").focus();
+//     inputRef.current.focus();
+//     value = 2;
+    
+//   }
+
+
+//   return <div>
+//     Signup
+//      <input ref={inputRef} type="text"></input>
+//      <input type="text"></input>
+//      <button onClick={focusOnInput}>Submit</button>
+//   </div>
+  
+  
+// }
+
+
+
+// Clock 
+
+function App(){
+  
+
 }
-function Layout() {
-  return (
-    <div style={{ height: "100vh", background: "green" }}>
-      <Header />
-      <div style={{ height: "90vh", background: "red" }}>
-        <Outlet />
-      </div>
-      Footer | Contact us
-    </div>
-  );
-}
 
-function Header() {
-  return (
-    <div>
-      <Link to="/">Allen</Link>|
-      <Link to="/neet/online-coaching-class-11">Class 11</Link>|
-      <Link to="/neet/online-coaching-class-12">Class 12</Link>
-    </div>
-  );
-}
+ export default App;
 
-function NoPage() {
-  return <div>Sorry Page not found</div>;
-}
 
-function LandingPage() {
-  return <div>Welcome to allen</div>;
-}
 
-function Class11Program() {
-  return <div>NEET programs from Class 11th</div>;
-}
 
-function Class12Program() {
-  const navigate = useNavigate();
-
-  function redirectUser() {
-    navigate("/");
-  }
-
-  return (
-    <div>
-      NEET programs from Class 12th
-      <button onClick={redirectUser}>GO back to Landing Page</button>
-    </div>
-  );
-}
-
-export default App;
