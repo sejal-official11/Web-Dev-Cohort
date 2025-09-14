0a9abb114463aa660f9d46843e6efcc0302137a1
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-09-14 18:58:06 +0530

    Adding the useRef

--- 10.1/react/src/App.jsx
+++ 10.1/react/src/App.jsx
@@ -126,12 +126,31 @@ import { useRef, useState } from "react";
   
 // }
 
-
-
 // Clock 
 
 function App(){
-  
+  const [currentCount, setCurrentCount] = useState(0);
+  // const [timer, setTimer] = useState(0)
+  const timer = useRef()
+
+  function startClock(){
+     let value = setInterval(function(){
+      setCurrentCount(count => count + 1)
+    }, 1000);
+    timer.current = value;
+  }
+
+
+  function stopClock(){
+    console.log(timer.current);
+    clearInterval(timer.current);
+  }
+
+   return <div>
+    {currentCount}
+    <button onClick={startClock}>Start</button>
+    <button onClick={stopClock}>Stop</button>
+   </div>
 
 }
 
