759e6806dc89465ab41bf407814ab0b1d29d67d7
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-09-16 16:38:52 +0530

    Use context api

--- 10.2/react/src/App.jsx
+++ 10.2/react/src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
+import { createContext, useContext, useState } from "react";
 
+const BulbContext = createContext();
 
-function App() {
+export function BulbProvider({ children }) {
+  const [bulbOn, setBulbOn] = useState(true);
 
-  return (
-   <div>
-    Hi there
-
-   </div>
-  )
+  return <BulbContext.Provider>{children}</BulbContext.Provider>;
 }
 
-export default App
+function App() {
+  <div>
+    <BulbProvider>
+      <Light /> // children
+    </BulbProvider>
+  </div>;
+}
+
+function Light({ bulbOn, setBulbOn }) {
+  return (
+    <div>
+      <LightBulb bulbOn={bulbOn} />
+      <LightSwitch bulbOn={bulbOn} setBulbOn={setBulbOn} />
+    </div>
+  );
+}
+
+function LightBulb() {
+  const { bulbOn } = useContext(BulbContext);
+  return <div>{bulbOn ? "Bulb On" : "Bulb Off"}</div>;
+}
+
+function LightSwitch({ bulbOn, setBulbOn }) {
+  function toggle() {
+    setBulbOn(!bulbOn);
+  }
+
+  return (
+    <div>
+      <button onClick={toggle}> Toggle the bulb</button>
+    </div>
+  );
+}
+
+export default App;
