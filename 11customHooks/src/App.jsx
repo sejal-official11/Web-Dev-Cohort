c5f0639410cbf18321d7d48ca1522ef928fdb093
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-09-29 21:47:18 +0530

    use custom hook

--- 11customHooks/src/App.jsx
+++ 11customHooks/src/App.jsx
@@ -1,46 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-
-
-
-function useCounter() {
-  const [count, setCount] = useState(0);
-
-  function increaseCount() {
-    // setCount(count + 1);
-    setCount((c) => c + 1);
-  }
-
-  return {
-    count: count,
-    increaseCount: increaseCount,
-  };
-}
 
 function App() {
+
+  const[post, setPost] = useState({});
+
+  async function getPosts() {
+     const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+     const json = await response.json();
+      setPost(json)
+  }
+     
+  useEffect(() => {
+    getPosts();
+   }, [])
 
 
   return (
     <div>
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
+     {post.title}
     </div>
   );
+
+
 }
 
 
-function Counter() {
-  const {count, increaseCount} = useCounter();
-  return (
-    <>
-      <div>
-        <button onClick={increaseCount}>Increase {count}</button>
-      </div>
-    </>
-  );
-}
 
 export default App;
