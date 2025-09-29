6d3d61a3147bb6155eab3b3d77178d735dc2be87
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-09-29 21:34:22 +0530

    Create custom hook

--- 11customHooks/src/App.jsx
+++ 11customHooks/src/App.jsx
@@ -1,17 +1,46 @@
-// import { useEffect, useState } fsrom "react";
-import { usePostTitle } from "./hooks/useFetch";
+import { useState } from "react";
 
 
-  function App() {
-  const postTitle = usePostTitle();
-  
+
+
+function useCounter() {
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    // setCount(count + 1);
+    setCount((c) => c + 1);
+  }
+
+  return {
+    count: count,
+    increaseCount: increaseCount,
+  };
+}
+
+function App() {
+
+
   return (
     <div>
-      {postTitle} HI there
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
     </div>
-  )
+  );
 }
 
 
+function Counter() {
+  const {count, increaseCount} = useCounter();
+  return (
+    <>
+      <div>
+        <button onClick={increaseCount}>Increase {count}</button>
+      </div>
+    </>
+  );
+}
 
 export default App;
