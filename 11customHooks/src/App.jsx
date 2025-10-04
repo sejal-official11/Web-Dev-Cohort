d371fcb99f822b704f5f63b0c038980cd7965527
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-10-04 16:09:31 +0530

    useDebounceHook

--- 11customHooks/src/App.jsx
+++ 11customHooks/src/App.jsx
@@ -1,37 +1,59 @@
 // import { useFetch } from "./hooks/useFetch";
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 // import usePrev from "./hooks/usePrev";
 
-import { useRef } from "react";
+// import { useRef } from "react";
 
+// function useDebounce(originalFn) {
+//   const currentClock = useRef();
 
+//   const fn = () => {
+//     setTimeout(currentClock.current);
+//     currentClock.current = setTimeout(originalFn, 200)
+//   }
 
-function useDebounce(originalFn) {
-  const currentClock = useRef();
-  
-  const fn = () => {
-    setTimeout(currentClock.current);
-    currentClock.current = setTimeout(originalFn, 200)
-  }
+//   return fn;
+// }
+//
 
-  return fn; 
-}
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
 
 function App() {
-  function sendDataToBackend() {
-    fetch("api.amazon.com/search/");
+  // function sendDataToBackend() {
+  //   fetch("api.amazon.com/search/");
+  // }
+
+  // const debouncedFn = useDebounce(sendDataToBackend)
+  const [inputValue, setInputValue] = useState("");
+  const debouncedValue = useDebounce(inputValue, 200);
+
+  function change(e) {
+    setInputValue(e.target.value);
   }
 
-  const debouncedFn = useDebounce(sendDataToBackend)
-
+  useEffect(() => {
+    console.log("expensive, operation");
+  }, [debouncedValue]);
 
   return (
     <>
-    <input type="text" onChange={debouncedFn}></input>
+      <input type="text" onChange={change}></input>
     </>
   );
-
 }
 
 export default App;
