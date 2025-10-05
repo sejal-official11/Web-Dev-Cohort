0f9740541747659447831e492c4ab24b5ed9f810
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-10-05 18:23:03 +0530

    Steps to import recoil

--- 11_Recoil/src/App.jsx
+++ 11_Recoil/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { counterAtom } from "./store/atoms/counter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <RecoilRoot>
+      <Counter />
+    </RecoilRoot>
+  );
 }
 
-export default App
+function Counter() {
+  return (
+    <div>
+      <CurrentCount />
+      <Increase />
+      <Decrease />
+    </div>
+  );
+}
+
+function CurrentCount() {
+  const count = useRecoilValue(counterAtom);
+  return <div>{count}</div>;
+}
+function Increase() {
+  const setCount = useSetRecoilState(counterAtom);
+
+  function increase() {
+    setCount((count) => count + 1);
+  }
+  return (
+    <div>
+      <button onClick={increase}>Increase</button>
+    </div>
+  );
+}
+
+function Decrease() {
+  const setCount = useSetRecoilState(counterAtom);
+
+  function decrease() {
+    setCount((count) => count - 1);
+  }
+  return (
+    <div>
+      <button onClick={decrease}>Decrease</button>
+    </div>
+  );
+}
+export default App;
