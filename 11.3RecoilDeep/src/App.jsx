d4a2411b40ed7ade52077ac4d90a5a7cba0d7c18
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-10-06 17:47:32 +0530

    recoit atoms

--- 11.3RecoilDeep/src/App.jsx
+++ 11.3RecoilDeep/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecoilRoot, useRecoilValue } from "recoil";
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from "./store/atoms";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificationAtomCount = useRecoilValue(notificationAtom)
+  const messagingAtomCount = useRecoilValue(messagingAtom)
 
-  return (
+  return(
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button>Home</button>
+    <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+    <button>Jobs ({jobsAtomCount})</button>
+    <button>Messaging ({messagingAtomCount})</button>
+    <button>Notifications ({notificationAtomCount})</button>
+    <button>Me</button>
     </>
+
+
   )
+
 }
 
-export default App
+
+export default App;
