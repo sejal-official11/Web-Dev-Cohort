563eebfeabd1a9b5dcf73dda10e9f262cbe27d00
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-10-06 19:58:46 +0530

    Adding atom family

---
+++ 11.3RecoilDeep/src/App.jsx
@@ -0,0 +1,37 @@
+
+
+import {RecoilRoot, atom, useRecoilValue} from 'recoil'
+
+
+const todoAtom = atom({
+  key: "todoAtom",
+  default: {
+    id: 1,
+    title: "Go to gym",
+    description: "Hit the gym from 7-9"
+  }
+})
+
+
+function App() {
+  
+  return(
+    <>
+    <RecoilRoot>
+      <Todo id={1}/>
+      <Todo id={2}/>
+    </RecoilRoot>  
+    </>
+  )
+}
+
+function Todo(){
+  const currentTodo = useRecoilValue(todoAtom);
+  <>
+  {currentTodo.title}
+  {currentTodo.description}
+    </>
+}
+
+
+export default App;
