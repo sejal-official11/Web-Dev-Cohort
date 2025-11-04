17a043e71d7b676769b1d0be235b715a8434d723
Sejal Gupta <sejalguptaofficial11@gmail.com>
2025-11-04 16:04:36 +0530

    ts basics

---
+++ 14-typescript/mee-14-ts/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// let x = 1;
+// console.log(x);
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 1;
+console.log(x);
+//# sourceMappingURL=index.js.map
---
+++ 14-typescript/mee-14-ts/index.ts
@@ -0,0 +1,78 @@
+
+
+// let x = 1;
+// console.log(x);
+
+
+// let x: number | string = 1;
+
+// console.log(x);
+
+// x = 'sejal';
+// console.log(x);
+
+
+
+// function greet(firstName: String) {
+//     console.log("Hello " + firstName);
+// }
+
+// greet("Sejal");
+
+
+// function sum(a: number, b: number): void {
+//     let num: number = a+b;
+//     console.log(num);
+// }
+
+// let ans = sum(3, 5);
+// console.log(ans);
+
+
+// function isLegal(age: number) {
+//     if(age > 18) return true;
+//     else return false;
+// }
+
+// console.log(isLegal(12));
+
+
+// function after1sec(fn: () => void){
+
+//     setTimeout(fn, 1000);
+
+// }
+
+
+// after1sec(function(){
+//     console.log("Hi there");
+// })
+
+
+
+// function delayed(fn: () => void) {
+//     setTimeout(fn, 1000);
+// }
+
+// function newFun(){
+//     console.log("Hello the new");
+// }
+
+// delayed(newFun);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
